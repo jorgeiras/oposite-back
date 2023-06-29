f056d5de91f64e81e6280f2ceca5643cf0835b28
jorgeiras <jorgeiras.alonso@hotmail.com>
2023-06-29 11:47:58 +0200

    cambio en debug

--- opositeback/settings.py
+++ opositeback/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8)3hil)#=ft2=5hy2_%#$=g!xvax*3vmqeh0@_!$n_@j3%h$dn'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
