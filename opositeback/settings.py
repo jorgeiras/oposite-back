f84cede4b5b9f376b1635f532117899bad252af5
jorgeiras <jorgeiras.alonso@hotmail.com>
2023-10-25 21:27:35 +0200

    cambios en los settings para conectar a mysql de pythonanywhere

--- opositeback/settings.py
+++ opositeback/settings.py
@@ -75,9 +75,13 @@ WSGI_APPLICATION = 'opositeback.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+   'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'jorgeiras$obfundb',
+        'USER': 'jorgeiras',
+        'PASSWORD': 'objetivofuncionario',
+        'HOST': 'jorgeiras.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
     }
 }
 
