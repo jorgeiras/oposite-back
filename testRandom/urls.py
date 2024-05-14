32432dffba22e8f1ec99dc4c35fcc985e9792685
jorgeiras <jorgeiras.alonso@hotmail.com>
2024-05-14 11:16:42 +0200

    fix register

--- testRandom/urls.py
+++ testRandom/urls.py
@@ -6,6 +6,6 @@ from .views import UserRegistrationAPIView
 
 urlpatterns = [
     path('generate_test/',generate_test , name='generate_test'),
-    path('register/', UserRegistrationAPIView.as_view(), name='register'),
+    path('register_user/', UserRegistrationAPIView.as_view(), name='register_user'),
     # Other URL patterns...
 ]
